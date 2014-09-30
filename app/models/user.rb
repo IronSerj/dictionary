2dabf58340f404f636a1547b86edff928abf2133
IronSerj <iron.serj@gmail.com>
2014-09-30 22:19:45 +0400

    Spaces fixed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ActiveRecord::Base
   acts_as_authentic
 
   def initialize(params)
-  	super(params)
-  	self.verification_token = SCrypt::Password.create(self.email)
+    super(params)
+    self.verification_token = SCrypt::Password.create(self.email)
   end
 
   def init_verification
-  	UserMailer.verification_email(self).deliver
+    UserMailer.verification_email(self).deliver
   end
 
 end
